c119a1ac50a67ea658bcb8dc3361055c196d074e
Randy Ylla <37916774+rylla@users.noreply.github.com>
2023-04-18 16:48:12 -0400

    Add files via upload

--- euler_projects/euler_1/euler_1.py
+++ euler_projects/euler_1/euler_1.py
@@ -1,4 +1,7 @@
 # Project Euler, Challenge 1.
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
 # Get multiples of 3 and 5 within range of numbers.
 # This also stores the multiples in a list and prints them.
 
